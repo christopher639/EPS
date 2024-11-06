09990fbfc10d6dbaa2724ec910d3c6dfffe23d2c
christopher639 <bundichristopher639@gmail.com>
2024-11-06 16:49:55 +0300

    Enables parents pages

--- frontend/src/Pages/Parents.jsx
+++ frontend/src/Pages/Parents.jsx
@@ -1,26 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Parents = () => {
+axios.defaults.baseURL = "https://eps-backend.onrender.com";
+
+const  Parents = () => {
+  const [students, setStudents] = useState([]);
+
+  const getFetchData = () => {
+    axios.get("https://eps-backend.onrender.com/api/students")
+      .then(response => setStudents(response.data))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getFetchData();
+  }, []);
+
+  // Function to format date as "DD/MM/YYYY"
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-GB", {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+  };
+
   return (
-    <div className='flex flex-col'>
-        <div className='flex mt-2 mb-2 flex-row justify-between'>
-            <p className='ml-4'>
-                <input className='outline-none border border-slate-500' type="text" placeholder='Search' />
-            </p>
-            <button className='bg-green-700 text-white rounded px-2 py-1 mx-2'>SEND MESSAGE</button>
-            <button className='bg-green-700 text-white rounded px-2 py-1 mx-2'>SEND EMAIL</button>
-        </div>
-    <table className='min-w-full'>
-        <thead className='bg-slate-800 text-white'>
-            <td className='border-r border-slate-500 whitespace-nowrap'>P/G NAME</td>
-            <td className='border-r border-slate-500 whitespace-nowrap'>EMAIL</td>
-            <td className='border-r border-slate-500'>PHONE NO</td>
-            <td className='border-r border-slate-500 whitespace-nowrap'>STD NAME</td>
-            <td className='border-r border-slate-500'>REG NO</td>
-        </thead>
-    </table>
+    <div className='flex flex-col min-w-full'>
+      <div className='flex max-h-[87vh] overflow-y-auto overflow-x-auto mr-5 md:mr-0'>
+        <table className='min-w-full mt-2'>
+          <thead className='bg-slate-800 px-1 text-white'>
+            <tr>
+              <th className='border whitespace-nowrap px-1'>NO</th>
+              <th className='border whitespace-nowrap px-1'>Full Name</th>
+              <th className='border whitespace-nowrap px-1'>EMAIL</th>
+              <th className='border whitespace-nowrap px-1'>PHONE</th>
+              <th className='border whitespace-nowrap px-1'>STUDENT </th>
+              <th className='border whitespace-nowrap px-1'>STREAM</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {
+              students.map((student, index) => (
+                <tr key={index} className='border  py-3 border-slate-500'>
+                  <td className='pr-2 whitespace-nowrap border border-slate-500'>{index + 1}</td>
+                  <td className='pr-2 whitespace-nowrap border border-slate-500'>{student.
+parentname}</td>
+                  <td className='pr-2 whitespace-nowrap border border-slate-500'>{student.
+email}</td>
+                  <td className='pr-2 whitespace-nowrap border border-slate-500'>{student.
+phone
+}</td>
+                  <td className='pr-2 whitespace-nowrap border text-center border-slate-500'>{student.name}</td>
+                  <td className='pr-2 whitespace-nowrap border border-slate-500'>{student.stream}</td>
+                 
+                 
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Parents
+export default Parents;
