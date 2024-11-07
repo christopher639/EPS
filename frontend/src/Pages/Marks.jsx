0095a94ecc3d7b52e6f6f75e738e76d102b4a960
christopher639 <bundichristopher639@gmail.com>
2024-11-07 19:58:53 +0300

    Fixed report form issues

--- frontend/src/Pages/Marks.jsx
+++ frontend/src/Pages/Marks.jsx
@@ -52,7 +52,7 @@ const Marks = () => {
                                 Print
                             </button>
                         </div>
-                        <NavLink to='/reportform1y'>
+                        <NavLink to='/reportform'>
                             <p className='text-center bg-green-600 py-2 px-4 text-white text-sm mb-2 rounded-md hover:bg-green-700'>
                                 Report
                             </p>
