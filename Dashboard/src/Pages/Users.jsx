158f64fb8a0531208589ce2862aefe728c0b9472
christopher639 <bundichristopher639@gmail.com>
2025-03-01 00:44:56 +0300

    made user serch input field max-w-40

--- Dashboard/src/Pages/Users.jsx
+++ Dashboard/src/Pages/Users.jsx
@@ -244,7 +244,7 @@ const Users = () => {
         </div>
         <div className="sm:hidden flex justify-between p-4">
           <input
-            className="outline-none px-4 py-2 border border-gray-300 rounded-md"
+            className="outline-none max-w-40 px-4 py-2 border border-gray-300 rounded-md"
             type="text"
             placeholder="Search"
             value={searchTerm}
